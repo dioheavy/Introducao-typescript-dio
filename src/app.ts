dc9cc1a9734ff924362264b4583092432890add3
Dioheavy <dioheavy@hotmail.com>
2022-07-06 11:00:19 -0300

    Commit dos desafios de Typescript da plataforma DIO

---
+++ src/app.ts
@@ -0,0 +1,121 @@
+
+//Desafio 1:
+
+/*let employee = {};
+employee.code = 10;
+employee.name = "John";*/
+
+const usuario = {
+    codigo: 198,
+    nome: 'André dos Santos'
+};
+
+
+
+//Desafio 2:
+
+/*let pessoa1 = {};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome = "carlos",
+    idade = 19,
+    profissao = "padeiro"
+}*/
+
+enum Profissao {
+    Guitarrista,
+    Baixista,
+    Baterista,
+    Vocalista
+}
+
+type Membro = {
+    nome: string,
+    idade: number,
+    profissao: Profissao
+}
+
+let membro1: Membro = {
+    nome: 'Andre',
+    idade: 42,
+    profissao: Profissao.Guitarrista
+};
+
+let membro2: Membro = {
+    nome: 'Alessandro',
+    idade: 48,
+    profissao: Profissao.Baterista
+};
+
+
+
+//Desafio 3:
+
+/*let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+campoSaldo.innerHTML = 0
+function somarAoSaldo(soma) {
+    campoSaldo.innerHTML += soma;
+}
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+}
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(soma.value);
+});
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});*/
+
+export {}
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo');
+let saldoTotal = 0
+
+limparSaldo()
+
+function somarAoSaldo(soma: number) {
+    if (campoSaldo) {
+        saldoTotal += soma
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+
+function limparCampoSoma() {
+    soma.value = "";
+}
+
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', () => {
+        somarAoSaldo(Number(soma.value)); 
+    });
+}
+botaoLimpar.addEventListener('click', () => {
+    limparSaldo();
+});
